3e7266ac254b843001cc6d1a027298da8039f7c4
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-01-28 02:44:29 -0800

    added the style for the Card component

---
+++ components/Card.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Card() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text>Card</Text>
+        <Text>Card</Text>
+        <Text>Card</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 6,
+    elevation: 3,
+    backgroundColor: "darkgrey",
+    shadowOffset: { width: 3, height: 3 },
+    shadowColor: "grey",
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+  },
+  content: {
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+});
