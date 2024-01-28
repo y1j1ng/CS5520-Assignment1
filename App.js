6a8d52cf68aaa374826c57eef5315c5ecb28e728
YIJING WU <yijingwu@YIJINGdeMacBook-Pro.local>
2024-01-27 18:05:07 -0800

    added the linear gradient background

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={["rgba(245, 245, 245, 0.8)", "transparent"]}
+        style={styles.background}
+      />
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +19,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "mediumorchid",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+    height: null,
   },
 });
